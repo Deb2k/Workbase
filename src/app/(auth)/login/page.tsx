c04e32280d57276932b1b1286c913ea1736d0b60
Deb2k <debashisphukan67@gmail.com>
2025-10-16 11:55:16 +0530

    added Protected Procedures

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from '@/featers/auth/components/login-form'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <LoginForm/>
+    </div>
+  )
+}
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from '@/featers/auth/components/register-from'
+
+
+export default function page() {
+  return (
+    <div>
+      <RegisterForm/>
+    </div>
+  )
+}
